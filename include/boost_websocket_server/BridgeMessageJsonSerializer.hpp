1be225f6fb0be79e7ddbccb2d6a09c79fa2d1125
Matthijs Besselink <matthijsbesselink@live.nl>
2021-01-12 12:44:49 +0100

    going to switch json libraries. Trying boost json instead of this. Polymorphism / generic types not supported.

--- include/boost_websocket_server/BridgeMessageJsonSerializer.hpp
+++ include/boost_websocket_server/BridgeMessageJsonSerializer.hpp
@@ -1,13 +1,33 @@
 #ifndef BRIDGEMESSAGEJSONSERIALIZER_HPP
 #define BRIDGEMESSAGEJSONSERIALIZER_HPP
 
-#include "BridgeMessages.hpp"
-#include "json.hpp"
+#include "BridgeMessageSerializer.hpp"  // Interface to implement.
+#include "BridgeMessages.hpp"           // Messages to parse.
 
-using nlohmann::json;
+#include <json/json.h>
 
-// https://github.com/nlohmann/json#basic-usage
+#include "json.hpp"                     // json parser, single header release: https://github.com/nlohmann/json
+using nlohmann::json;   
 
+template<typename T>
+T* tryAt_optional_value(const json& j, const std::string name)
+{
+    try
+    {
+        j.at(name);
+
+        auto val = new T();
+        j.at(name).get_to(val);
+        return val;
+    }
+    catch(const std::out_of_range& e) 
+    {
+        return nullptr;
+    }
+    
+}
+
+// method used: https://github.com/nlohmann/json#basic-usage
 namespace BridgeMessages 
 {
     void to_json( json& j, const SetStatusLevel& m) 
@@ -15,23 +35,44 @@ namespace BridgeMessages
         // Using ternary operator for conditional assignment of optionally initialized/used parameters.
         // https://www.tutorialspoint.com/c-cplusplus-ternary-operator
         j = json{{"op", m.op}, {"id", m.id ? *m.id : "NULL" }, {"level", m.level} };
+
     }
 
     void from_json(const json& j, SetStatusLevel& m)
     {
-        j.at("id").get_to(*m.id); // What am i supposed to do here??
+        // Check if ID exists.
+        m.id = tryAt_optional_value<std::string>(j, "id");
         j.at("level").get_to(m.level);
     }
 };
 
 
-class BridgeMessageJsonSerializer
+class BridgeMessageJsonSerializer : public BridgeMessageSerializer
 {
 private:
     /* data */
 public:
     BridgeMessageJsonSerializer(/* args */) {}
     ~BridgeMessageJsonSerializer() {}
+
+    /**
+     * @brief Serializes a given datatype that has to be of subset BridgeMessages
+     * 
+     * @tparam BridgeMessage 
+     * @param message 
+     * @return std::string 
+     */
+    template<typename BridgeMessage>
+    std::string Serialize(BridgeMessage& message)
+    {
+        return json(message).dump();
+    }
+
+    
+    std::unique_ptr<BridgeMessage> Deserialize(std::string data)
+    {
+        from_json(json(data), data);
+    }
 };
 
 
